cbe349cd163fe2346a5d249b70a48a75539a20dd
saviya98 <savithdissanayake98@gmail.com>
2023-01-05 00:40:40 +0530

    post api created

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/getUser")
     public String getUser(){
         return "Savith";
     }
     @PostMapping("/saveUser")
-    public String saveUser(){
-        return "User Saved";
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
     }
     @PutMapping("/updateUser")
     public String updateUser(){
